acc14d261a91d16cb4fbb3df5a521d445eb4f0b5
DAEHYEONJO <jdhqpt12@gmail.com>
2021-04-20 18:55:26 +0900

    midterm commit

---
+++ SearchIR/src/parse/genSnippet.java
@@ -0,0 +1,50 @@
+package parse;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class genSnippet {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File file=new File("/Users/jodaehyeon/Desktop/SimpleIR/SearchIR/src/data/"+args[3]);
+		ArrayList input=new ArrayList<String>();
+		String str=args[5];
+		String[] strline=str.split(" ");
+		
+		Scanner scan=new Scanner(file);
+		ArrayList list=new ArrayList<String>();
+		ArrayList count=new ArrayList<Integer>();
+		while(scan.hasNextLine()) {
+			list.add(scan.nextLine());
+		}
+		
+		for(int i=0;i<list.size();i++) {
+			int c=0;
+			for(int j=0;j<strline.length;j++) {
+				if(list.get(i).toString().contains(strline[j])) {
+					c++;
+				}
+			}
+			count.add(i, c);
+		}
+		
+		for(int i=0;i<count.size();i++) {
+			System.out.println(count.get(i));
+		}
+		
+		int temp=(int)count.get(0);
+		for(int k=0;k<count.size()-1;k++) {
+			
+			if(temp<=(int)count.get(k+1)) {
+				temp=(int)count.get(k+1);
+				System.out.println("t"+temp);
+			}
+		}
+		System.out.println("t"+temp);
+		System.out.println(list.get(temp-1));
+	}
+
+}
